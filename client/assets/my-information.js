7e97b2bc6d1b555ba6984484a5c02bbe16eaaeb8
Cat-in-box <69214647+Cat-in-box@users.noreply.github.com>
2021-05-04 01:47:01 +0300

    добавили пользовательский выбор confirm и немного улучшили условия

--- client/assets/my-information.js
+++ client/assets/my-information.js
@@ -4,72 +4,72 @@ $(document).ready(function(){
 	if (user == null) {
 		alert("Чтобы воспользоваться сервисом, ВОЙДИТЕ или ЗАРЕГИСТРИРУЙТЕСЬ");
 		window.location.replace("index.html");
-	}
+	} else {
 	
-	informationFill()
-	function informationFill() {
-		$.ajax({
-			url: "http://localhost:8080/clients/info/" + user,
-			type: "GET",
-			dataType: "json",
-			success: function (response){
-				document.getElementById('email').textContent = response["email"];
-				document.getElementById('password').textContent = response["userPassword"];
-				document.getElementById('passport').placeholder = response["passport"];
-				document.getElementById('last-name').placeholder = response["lastName"];
-				document.getElementById('first-name').placeholder = response["firstName"];
-				document.getElementById('patronymic').placeholder = response["patronymic"];
-				document.getElementById('date-of-birth').placeholder = response["dateOfBirth"];
-				document.getElementById('phone-number').placeholder = response["phoneNumber"];
-			},
-			error: function(response) {
+		informationFill()
+		function informationFill() {
+			$.ajax({
+				url: "http://localhost:8080/clients/info/" + user,
+				type: "GET",
+				dataType: "json",
+				success: function (response){
+					document.getElementById('email').textContent = response["email"];
+					document.getElementById('password').textContent = response["userPassword"];
+					document.getElementById('passport').placeholder = response["passport"];
+					document.getElementById('last-name').placeholder = response["lastName"];
+					document.getElementById('first-name').placeholder = response["firstName"];
+					document.getElementById('patronymic').placeholder = response["patronymic"];
+					document.getElementById('date-of-birth').placeholder = response["dateOfBirth"];
+					document.getElementById('phone-number').placeholder = response["phoneNumber"];
+				},
+				error: function(response) {
+					console.log("Что-то пошло не так", error);
+				}
+			});
+		}
+
+		$("#b-change").click(async function () {
+			let varData = {
+				"passport": document.getElementById('passport').value,
+				"lastName": document.getElementById('last-name').value,
+				"firstName": document.getElementById('first-name').value,
+				"patronymic": document.getElementById('patronymic').value,
+				"dateOfBirth": document.getElementById('date-of-birth').value,
+				"phoneNumber": document.getElementById('phone-number').value
+			};
+			let response = await fetch("http://localhost:8080/clients/update/" + user, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8'
+				},
+				body: JSON.stringify(varData)
+			}, {mode: 'cors'}).then(function (response) {
+
+				if (response.status === 400) {
+					alert("Введены некорректные данные")
+				} else if (response.status === 200) {
+					alert("Ваши данные изменены!")
+					document.getElementById('passport').value = "";
+					document.getElementById('last-name').value = "";
+					document.getElementById('first-name').value = "";
+					document.getElementById('patronymic').value = "";
+					document.getElementById('date-of-birth').value = "";
+					document.getElementById('phone-number').value = "";
+
+					informationFill()
+					
+				} else {
+					alert("Пролучили код " + response.status)
+				}
+
+			}).catch(function (error) {
 				console.log("Что-то пошло не так", error);
-			}
+			});
+		});
+
+		$("#b-exit").click(function () {
+			eraseCookie("user-id");
+			window.location.replace("index.html");
 		});
 	}
-
-	$("#b-change").click(async function () {
-        let varData = {
-            "passport": document.getElementById('passport').value,
-            "lastName": document.getElementById('last-name').value,
-			"firstName": document.getElementById('first-name').value,
-			"patronymic": document.getElementById('patronymic').value,
-			"dateOfBirth": document.getElementById('date-of-birth').value,
-			"phoneNumber": document.getElementById('phone-number').value
-        };
-        let response = await fetch("http://localhost:8080/clients/update/" + user, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(varData)
-        }, {mode: 'cors'}).then(function (response) {
-
-            if (response.status === 400) {
-                alert("Введены некорректные данные")
-            } else if (response.status === 200) {
-                alert("Ваши данные изменены!")
-				document.getElementById('passport').value = "";
-            	document.getElementById('last-name').value = "";
-				document.getElementById('first-name').value = "";
-				document.getElementById('patronymic').value = "";
-				document.getElementById('date-of-birth').value = "";
-				document.getElementById('phone-number').value = "";
-
-				informationFill()
-                
-            } else {
-                alert("Пролучили код " + response.status)
-            }
-
-        }).catch(function (error) {
-            console.log("Что-то пошло не так", error);
-        });
-    });
-
-	$("#b-exit").click(function () {
-		eraseCookie("user-id");
-		window.location.replace("index.html");
-	});
-	
 });
--- client/assets/schedule.js
+++ client/assets/schedule.js
@@ -4,123 +4,120 @@ $(document).ready(function(){
 	if (user == null) {
 		alert("Чтобы воспользоваться сервисом, ВОЙДИТЕ или ЗАРЕГИСТРИРУЙТЕСЬ");
 		window.location.replace("index.html");
-	}
+	} else {
 
-	$(document).ready(function(){
 		stationToFill()
 		dateFill()
 		getSchedule()
-	});
 
-	function clicker(rowNumber) {
-		let currentTr = document.querySelectorAll('#t-schedule tr')[rowNumber];
-		setCookie("selected-schedule-id", currentTr.cells[0].textContent, "10");
+		function stationToFill() {
+			$.ajax({
+				url: "http://localhost:8080/stations/getAllNames",
+				type: "GET",
+				dataType: "json",
+				success: function (response){
+					select = document.getElementById('stations-to');
 
-		window.location.replace("buying.html");
-    	};
+					for (let i = 0; i<response.length; i++){
+						let opt = document.createElement('option');
+						opt.value = i;
+						opt.innerHTML = response[i];
+						select.appendChild(opt);
 
-	function stationToFill() {
-		$.ajax({
-			url: "http://localhost:8080/stations/getAllNames",
-			type: "GET",
-			dataType: "json",
-			success: function (response){
-				select = document.getElementById('stations-to');
-
-				for (let i = 0; i<response.length; i++){
-					let opt = document.createElement('option');
-					opt.value = i;
-					opt.innerHTML = response[i];
-					select.appendChild(opt);
-
-				}
-
-			},
-			error: function(response) {
-				console.log("Что-то пошло не так", error);
-			}
-		});
-	}
-
-	function dateFill() {
-		$.ajax({
-			url: "http://localhost:8080/schedules/getAllDates",
-			type: "GET",
-			dataType: "json",
-			success: function (response){
-				select = document.getElementById('date');
-
-				for (let i = 0; i<response.length; i++){
-					let opt = document.createElement('option');
-					opt.value = i;
-					opt.innerHTML = response[i];
-					select.appendChild(opt);
-
-				}
-
-			},
-			error: function(response) {
-				console.log("Что-то пошло не так", error);
-			}
-		});
-	}
-
-	function getSchedule() {
-		let fromStation = document.getElementById('stations-from').options[document.getElementById('stations-from').selectedIndex].text
-		let toStation = document.getElementById('stations-to').options[document.getElementById('stations-to').selectedIndex].text;
-		let tripDate = document.getElementById('date').options[document.getElementById('date').selectedIndex].text;
-		$.ajax({
-			url: "http://localhost:8080/schedules/getSchedule/" + fromStation + "/" + toStation + "/" + tripDate,
-			type: "GET",
-			dataType: "json",
-			success: function (response){
-				let colCount = $('#t-schedule th').length;
-
-				const table = document.getElementById("t-schedule");
-
-				for (let i = 0; i < response.length; i++) {
-					let row = table.insertRow(i + 1);
-					row.addEventListener('click', clicker.bind(null, i + 1));
-					for (let j = 0; j < colCount; j++) {
-						let cell = row.insertCell(j);
-						cell.innerHTML = response[i][j];
 					}
+
+				},
+				error: function(error) {
+					console.log("Что-то пошло не так", error);
 				}
-				
-				let rowCount = table.rows.length;
-				table.deleteRow(rowCount - 1);
-				
-			},
-			error: function(response) {
-				let td = document.querySelectorAll('#t-schedule td');
-				td[0].textContent = "Билетов не найдено"
+			});
+		}
 
+		function dateFill() {
+			$.ajax({
+				url: "http://localhost:8080/schedules/getAllDates",
+				type: "GET",
+				dataType: "json",
+				success: function (response){
+					select = document.getElementById('date');
+
+					for (let i = 0; i<response.length; i++){
+						let opt = document.createElement('option');
+						opt.value = i;
+						opt.innerHTML = response[i];
+						select.appendChild(opt);
+
+					}
+
+				},
+				error: function(response) {
+					console.log("Что-то пошло не так", error);
+				}
+			});
+		}
+
+		function getSchedule() {
+			let fromStation = document.getElementById('stations-from').options[document.getElementById('stations-from').selectedIndex].text
+			let toStation = document.getElementById('stations-to').options[document.getElementById('stations-to').selectedIndex].text;
+			let tripDate = document.getElementById('date').options[document.getElementById('date').selectedIndex].text;
+			$.ajax({
+				url: "http://localhost:8080/schedules/getSchedule/" + fromStation + "/" + toStation + "/" + tripDate,
+				type: "GET",
+				dataType: "json",
+				success: function (response){
+					let colCount = $('#t-schedule th').length;
+
+					const table = document.getElementById("t-schedule");
+
+					for (let i = 0; i < response.length; i++) {
+						let row = table.insertRow(i + 1);
+						row.addEventListener('click', clicker.bind(null, i + 1));
+						for (let j = 0; j < colCount; j++) {
+							let cell = row.insertCell(j);
+							cell.innerHTML = response[i][j];
+						}
+					}
+					
+					let rowCount = table.rows.length;
+					table.deleteRow(rowCount - 1);
+					
+				},
+				error: function(response) {
+					let td = document.querySelectorAll('#t-schedule td');
+					td[0].textContent = "Билетов не найдено"
+
+				}
+			});
+		}
+
+		function clicker(rowNumber) {
+			let currentTr = document.querySelectorAll('#t-schedule tr')[rowNumber];
+			setCookie("selected-schedule-id", currentTr.cells[0].textContent, "10");
+
+			window.location.replace("buying.html");
+			};
+
+		$("#stations-to").change(function(){
+			refillSchedule();
+		})
+
+		$("#date").change(function(){
+			refillSchedule();
+		})
+
+		function refillSchedule() {
+			const table = document.getElementById("t-schedule");
+			while (table.rows.length > 2) {
+				table.deleteRow(table.rows.length - 1);
 			}
-		});
-	}
 
-	$("#stations-to").change(function(){
-		refillSchedule();
-	})
+			let td = document.querySelectorAll('#t-schedule td');
 
-	$("#date").change(function(){
-		refillSchedule();
-	})
-
-	function refillSchedule() {
-		const table = document.getElementById("t-schedule");
-		while (table.rows.length > 2) {
-			table.deleteRow(table.rows.length - 1);
+			for (let i = 0; i < td.length; i++) {
+				td[i].textContent = "";
+			}
+			
+			getSchedule();
 		}
-
-		let td = document.querySelectorAll('#t-schedule td');
-		td[0].textContent = "Билетов не найдено"
-
-		for (let i = 0; i < td.length; i++) {
-			td[i].textContent = "";
-		}
-		
-		getSchedule();
 	}
-	
 });
